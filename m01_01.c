d33e2ba9c63c8d3449dc37c4d7ffc7b4aa0e9fa5
Sitogab Antonio <antonio.girsang@gmail.com>
2022-03-21 20:28:40 +0700

    menyelesaikan permasalahan

--- m01_01.c
+++ m01_01.c
@@ -23,7 +23,7 @@ int main(){
     do
     {
         scanf("\n");
-        scanf(input);
+        gets(input);
         if (strstr(input, "student-show-all")!=NULL) {
             for (int j = 0; j < jlhmahasiswa; j++)
             {
